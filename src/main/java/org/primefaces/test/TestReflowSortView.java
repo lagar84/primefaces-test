8a9dd64b9e3a3ba7a9cab40ec15952f6efc1550f
Euclides Melo <lagar84@users.noreply.github.com>
2019-01-10 20:10:11 -0200

    reproducer problem sort in datatable reflow mode

---
+++ src/main/java/org/primefaces/test/TestReflowSortView.java
@@ -0,0 +1,51 @@
+package org.primefaces.test;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "testReflowSortView")
+@ViewScoped
+public class TestReflowSortView implements Serializable {
+     
+	private static final long serialVersionUID = 1L;
+	
+	private Record selRow;
+    private List<Record> rows;
+    
+    @PostConstruct  
+    public void init() {
+    	rows = new ArrayList<>();        
+    	
+        rows.add(new Record(1, "Mojarra"));
+        rows.add(new Record(2, "Myfaces"));
+		rows.add(new Record(3, "Eclipselink"));
+		rows.add(new Record(4, "Hibernate"));
+		rows.add(new Record(5, "Spring"));        
+		rows.add(new Record(6, "Primefaces"));        
+    }
+
+	public Record getSelRow() {
+		return selRow;
+	}
+
+	public void setSelRow(Record selRow) {
+		this.selRow = selRow;
+	}
+
+	public List<Record> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<Record> rows) {
+		this.rows = rows;
+	}
+    
+    
+   
+	
+}
